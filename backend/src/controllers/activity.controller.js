cd3451d45e2d09add1c7a60855dab3556e65d207
Anjali <anjali@Anjalis-MacBook-Air.local>
2026-06-12 22:01:07 +0530

    removed log

--- backend/src/controllers/activity.controller.js
+++ backend/src/controllers/activity.controller.js
@@ -2,10 +2,7 @@ const Activity = require("../models/activity.model");
 const asyncHandler = require("../utils/asyncHandler");
 
 const getProjectActivity = asyncHandler(async (req, res) => {
-  const activities = await Activity.find({
-    project: req.project._id,
-    user: req.user.userId,
-  })
+  const activities = await Activity.find({ project: req.project._id })
     .populate("user", "name email")
     .sort({ createdAt: -1 });
 
